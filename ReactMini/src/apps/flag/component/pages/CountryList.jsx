804e2af279b9facb751d305330e7de7fc2993376
Suraj kr. Gupta <64039192+surajsohigh@users.noreply.github.com>
2024-04-23 09:45:08 +0530

    Update CountryList.jsx

--- ReactMini/src/apps/flag/component/pages/CountryList.jsx
+++ ReactMini/src/apps/flag/component/pages/CountryList.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 
 function CountryList({ query }) {
   return (
-    <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-5 mt-10 items-center justify-items-center">
+    <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 mt-10 items-center justify-items-center">
       {country
         .filter((item) => item.name.common.toLowerCase().includes(query))
         .map((item) => {
